941b91cdfbb88b765973392b59131b3ee4ec7cfb
sarahallaireSDV <sarah.allaire@supdevinci-edu.fr>
2024-10-24 22:00:35 +0200

    Indexation et commentaires

--- front/src/views/Layout.js
+++ front/src/views/Layout.js
@@ -8,17 +8,33 @@ import { HiOutlineInformationCircle } from "react-icons/hi";
 import { TbLogout } from "react-icons/tb";
 import '../css/Layout.css';
 
+// ---------------------------------------------------------------------------------------------
+// Layout est le template des différentes pages
+// On retrouvera le même fond, le header et le menu sur toutes les pages
+// Sauf pour la connexion et l'inscription
+// ---------------------------------------------------------------------------------------------
 const Layout = ({ children }) => {
 
-    const navigate = useNavigate();
+    // -----------------------------------------------------------------------------------------
+    // Déclarations constantes
+    // -----------------------------------------------------------------------------------------
 
+    const navigate = useNavigate();
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+    // -----------------------------------------------------------------------------------------
+    // Fonctions
+    // -----------------------------------------------------------------------------------------
+
+    // handleNavigation est appelée lorsqu'un item du menu est cliqué
+    // La fonction redirige vers la bonne page
     const handleNavigation = (path) => {
         navigate(path);
         setIsMenuOpen(false);
     };
 
+    // toggleMenu est appelée sur l'icon menu
+    // La fonction ouvre le menu si l'icon est cliqué
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
@@ -26,14 +42,14 @@ const Layout = ({ children }) => {
     
     return (
         <div>
-            {/* Header ou barre de navigation */}
+            {/* Header */}
             <header className='header'>
                 <div className='menu-icon' onClick={toggleMenu}>
                     <TiThMenu className='menu-icon'/>
                 </div>
                 <h2 onClick={() => handleNavigation('/Dashboard')}>RareCaching</h2>
 
-
+                {/* Menu */}
                 <div className={`side-menu ${isMenuOpen ? 'open' : ''}`}>
                     <ul>
                         <li onClick={() => handleNavigation('/Dashboard')} className='menu-item'>
@@ -66,13 +82,14 @@ const Layout = ({ children }) => {
                                 <span className='menu-item-label logout'>Déconnexion</span>
                             </div>
                         </li>
-
                     </ul>
                 </div> 
  
+                {/* Affichage du menu */}
                 {isMenuOpen && <div className="overlay" onClick={toggleMenu}></div>}  
             </header>
 
+            {/* Corps de la page */}
             <main>
                 {children}
             </main>
