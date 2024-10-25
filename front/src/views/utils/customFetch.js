4dc69ec8c843ebd68da1b9f0192d95f745f87cbf
Mohamed Reda LOUKHNATI <medredaloukhnati2001@gmail.com>
2024-10-25 19:48:53 +0200

    victory popup rework

--- front/src/views/utils/customFetch.js
+++ front/src/views/utils/customFetch.js
@@ -13,20 +13,6 @@ export async function customGetAllFetch(endpoint) {
     }
   };
 
-  export async function customGetAllFetch(endpoint) {
-    try {
-      const response = await fetch(`http://localhost:5000/${endpoint}`);  
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-        return data
-      } else {
-        console.error('Erreur lors de la récupération des parties');
-      }
-    } catch (error) {
-      console.error('Erreur réseau:', error);
-    }
-  };
   
   export async function customGetByIdFetch(endpoint, id) {
     try {
